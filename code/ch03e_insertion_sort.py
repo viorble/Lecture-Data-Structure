dc1e391cfb84ed224d84798b5ebf8ffe1413bf17
Jacky Lin <jacky.jw.lin@nycu.edu.tw>
2025-01-23 23:24:44 +0800

    add insertion sort

---
+++ code/ch03e_insertion_sort.py
@@ -0,0 +1,16 @@
+my_arr = [2, 4, 1, 5, 3]
+
+# insertion sort
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
+new_arr = insertion_sort(my_arr)
+print(','.join(map(str, new_arr)))  
